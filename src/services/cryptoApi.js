6681fcfc6c3669ed77c0b8e06f7cac43eeafbfe0
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2022-08-02 17:53:12 -0300

    some small changes

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '97b8b3c56cmsh944cf74ca07f777p1f66f1jsn717108036efb',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`coin/${coinId}`)
+    })
+  }),
+})
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi
